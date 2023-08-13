ff86a7ce89b62c0d83d19d5462eb88711b827bfb
YuLong <124372247+GumayuLong@users.noreply.github.com>
2023-08-13 12:23:56 +0700

    done

--- src/components/BaiTapGlasses/ListGlasses.jsx
+++ src/components/BaiTapGlasses/ListGlasses.jsx
@@ -1,40 +1,29 @@
 import React, { Component } from 'react'
-
-import Model from './Model'
-// import ListGlasses from './ListGlasses'
-
 import data from "../../data/dataGlasses.json"
 import "./ListGlasses.css";
 
-export default class BaiTapGlasses extends Component {
-    state = {
-        urlImage: "./glassesImage/v1.png",
-    }
 
-    handleOnClick = (imgName)=>{
-        this.setState({urlImage: imgName});
-    }
-
+export default class ListGlasses extends Component {
     renderItem = () => {
         return data.map((item, index) =>{
             return(
                 <div className='GlassesItem__container'>
-                    <div className='GlassesItem__img' key= {index} onClick={()=>{this.handleOnClick(item.url)}}>
+                    <div className='GlassesItem__img' key= {index}>
                         <img src = {item.url} alt="#"></img>
                     </div>
                 </div>
             )
         })
     }
+
     render() {
         return (
             <>
-            {console.log(this.state.urlImage)}
-                <Model props={this.state.urlImage}/>
                 <div className='ListGlasses__container'>
                     {this.renderItem()}
                 </div>
-            </>
+                
+            </>    
         )
     }
 }
--- src/components/BaiTapGlasses/Model.jsx
+++ src/components/BaiTapGlasses/Model.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-
+import "./Model.css";
 export default class Model extends Component {
     render() {
         return (
-        <div>
-            
-        </div>
+            <>
+            <div className='Model__container'>
+                <div className='Model__wrapper'>
+                    <div className='Model__human'>
+                        <img src = "./glassesImage/model.jpg" alt = "#"></img>
+                    </div>
+                    <div className='Model__glass'>
+                        <img src = {this.props.props} alt = "#"></img>
+                    </div>
+                </div>
+            </div>
+            </>
         )
     }
 }
